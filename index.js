dcadb11d967bdfbb35befc1e6e4cad946e2530d4
Imraninrcm <imraninrcm@gmail.com>
2026-02-10 15:36:33 +0530

    password copy issue solved

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const generator = require("generate-password");
 const app = express();
 const path = require("path");
-const ExpressError = require("./utils/ExpressError");
-const wrapAsync = require("./utils/wrapAsync");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -16,47 +14,22 @@ app.get("/", (req, res) => {
 });
 
 // Password generation route with proper error handling
-app.post(
-  "/password",
-  wrapAsync(async (req, res) => {
-    const pw = req.body.pw;
+app.post("/password", async (req, res) => {
+  const { pw } = req.body;
 
-    // Check if all options are "no"
-    if (
-      pw.numbers === "no" &&
-      pw.symbols === "no" &&
-      pw.lowercase === "no" &&
-      pw.uppercase === "no"
-    ) {
-      throw new ExpressError(400, "Please select at least one character type.");
-    }
+  //Generate password
+  const password = generator.generate({
+    length: Number(pw.length),
+    numbers: pw.numbers,
+    symbols: pw.symbols,
+    lowercase: pw.lowercase,
+    uppercase: pw.uppercase,
+    excludeSimilarCharacters: pw.excludeSimilarCharacters,
+    exclude: pw.exclude,
+    strict: pw.strict,
+  });
 
-    // Generate password
-    const password = generator.generate({
-      length: Number(pw.length),
-      numbers: pw.numbers === "yes",
-      symbols: pw.symbols === "yes",
-      lowercase: pw.lowercase === "yes",
-      uppercase: pw.uppercase === "yes",
-      excludeSimilarCharacters: pw.excludeSimilarCharacters === "yes",
-      exclude: pw.exclude,
-      strict: pw.strict === "yes",
-    });
-
-    res.render("password.ejs", { password });
-  })
-);
-
-// Catch-all route
-app.all("*", (req, res, next) => {
-  next(new ExpressError(404, "Page not found"));
-});
-
-// Global error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  const { status = 500, message = "Something went wrong" } = err;
-  res.status(status).render("error.ejs", { err });
+  res.render("password.ejs", { password });
 });
 
 // Server port
